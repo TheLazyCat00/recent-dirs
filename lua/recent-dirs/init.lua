05454d32f7970a2dbecbaf0350e3f4dade9c39e9
Manuel Stieger <manue.stieger@gmail.com>
2025-08-26 17:48:04 +0200

    initial commit

---
+++ lua/recent-dirs/init.lua
@@ -0,0 +1,156 @@
+local M = {}
+
+local function save_cwd()
+	local filepath = vim.fn.stdpath("data") .. "/recent-dirs"
+	local lines = vim.fn.readfile(filepath)
+
+	local cwd = vim.fn.getcwd():gsub("\\", "/")
+	local cur_buf_path = vim.api.nvim_buf_get_name(0):gsub("\\", "/")
+	local line_data = cwd .. "" .. cur_buf_path
+	for i, value in ipairs(lines) do
+		if value:match("(.-)") == cwd then
+			table.remove(lines, i)
+			break
+		end
+	end
+
+	table.insert(lines, 1, line_data)
+	vim.fn.writefile(lines, filepath)
+end
+
+---@return snacks.picker.Config
+local function recent_dirs()
+	return {
+
+		finder = function ()
+			local filepath = vim.fn.stdpath("data") .. "/recent-dirs"
+			local lines = vim.fn.readfile(filepath)
+
+			local items = {}
+			for i, line in ipairs(lines) do
+				local path = line:match("(.-)")
+				local buffer = line:match("(.*)")
+
+				table.insert(items, {
+					file = path,
+					text = path,
+					dir = true,
+					buffer = buffer,
+				})
+			end
+			return items
+		end,
+		actions = {
+			---@param picker snacks.Picker
+			---@param item snacks.picker.Item
+			confirm = function(picker, item)
+				picker:close()
+				if not item then
+					return
+				end
+
+				M.open_buffer(item.idx)
+
+				save_cwd()
+			end,
+			---@param picker snacks.Picker
+			---@param item snacks.picker.Item
+			delete = function(picker, item)
+				local done = {}
+				local deleted = {}
+
+				local filepath = vim.fn.stdpath("data") .. "/recent-dirs"
+				local lines = vim.fn.readfile(filepath)
+				table.remove(lines, item.idx)
+				vim.fn.writefile(lines, filepath)
+
+				deleted["idx"] = item.idx
+				picker:find({
+					on_done = function()
+						if picker:count() == 0 then
+							picker:close()
+						else
+							for _, it in ipairs(picker.list.items) do
+								done[#done + 1] = it.idx
+							end
+							if not vim.tbl_contains(done, deleted.idx) then
+								item.idx = item.idx - 1
+							end
+							picker.list:view(item.idx)
+						end
+					end,
+				})
+			end
+		},
+		win = {
+			input = {
+				keys = {
+					["<c-x>"] = { "delete", mode = { "n", "i" } },
+				},
+			},
+		},
+		format = Snacks.picker.format.file,
+		preview = Snacks.picker.preview.file,
+		title = "Recent Dirs"
+	}
+end
+
+function M.open_buffer(idx)
+	local filepath = vim.fn.stdpath("data") .. "/recent-dirs"
+	local lines = vim.fn.readfile(filepath)
+	if lines[idx] then
+		local line = lines[idx]
+
+		local path = line:match("(.-)")
+		local buffer = line:match("(.*)")
+		vim.fn.chdir(path)
+		vim.cmd("edit " .. buffer)
+
+		local bufs = vim.api.nvim_list_bufs()
+		for _, bufnr in ipairs(bufs) do
+			if bufnr ~= vim.api.nvim_get_current_buf() then
+
+				-- Check if the buffer is valid and listed before attempting to delete
+				if vim.api.nvim_buf_is_valid(bufnr) and vim.api.nvim_get_option_value("buflisted", { buf = bufnr }) then
+					-- Attempt to delete the buffer.
+					-- `force = false` (default) prevents deleting buffers with unsaved changes.
+					-- To discard changes and delete, you can set `force = true`.
+					-- pcall is used to silently ignore errors if a buffer can't be deleted (e.g., unsaved changes with force=false).
+					pcall(vim.api.nvim_buf_delete, bufnr, { force = false })
+				end
+			end
+		end
+	end
+end
+
+function M.open_dir(idx)
+	local filepath = vim.fn.stdpath("data") .. "/recent-dirs"
+	local lines = vim.fn.readfile(filepath)
+	if lines[idx] then
+		local line = lines[idx]
+
+		local path = line:match("(.-)")
+		vim.fn.chdir(path)
+	end
+end
+
+function M.load_recent()
+	M.open_dir(1)
+end
+
+function M.pick()
+	Snacks.picker.pick(recent_dirs())
+end
+
+function M.setup()
+end
+
+vim.api.nvim_create_autocmd("VimLeave", {
+	callback = save_cwd
+})
+
+vim.api.nvim_create_autocmd("DirChangedPre", {
+	callback = save_cwd
+})
+
+return M
